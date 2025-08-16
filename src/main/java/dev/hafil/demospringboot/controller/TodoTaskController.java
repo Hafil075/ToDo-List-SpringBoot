a49a8c513326a33b9c5e20235bf789614c81266d
hafil075 <hafilasfak@gmail.com>
2025-08-16 11:42:36 +0530

    updated todo task controller

--- src/main/java/dev/hafil/demospringboot/controller/TodoTaskController.java
+++ src/main/java/dev/hafil/demospringboot/controller/TodoTaskController.java
@@ -3,47 +3,53 @@ package dev.hafil.demospringboot.controller;
 import dev.hafil.demospringboot.model.TodoTask;
 import dev.hafil.demospringboot.service.TodoTaskServices;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/tasks")
 @RequiredArgsConstructor
 public class TodoTaskController {
-    @Autowired
-    private TodoTaskServices todoTaskServices;
+
+    private final TodoTaskServices todoTaskServices;
 
     @GetMapping
-    public List<TodoTask> getAllTasks() {
-        return todoTaskServices.findAll().getBody();
+    public ResponseEntity<List<TodoTask>> getAllTasks() {
+        return todoTaskServices.findAll();
     }
 
-    @GetMapping("/getAllby")
-    public List<TodoTask> getAllBy(@RequestParam long id) {
-        return todoTaskServices.findAllByCreator(id);
+    @GetMapping("/user/{userId}")
+    public ResponseEntity<List<TodoTask>> getAllTasksByUser(@PathVariable long userId) {
+        List<TodoTask> tasks = todoTaskServices.findAllByCreator(userId);
+        return ResponseEntity.ok(tasks);
     }
 
-    @GetMapping("/getOne")
-    public TodoTask getOne(@RequestParam long id) {
-        return todoTaskServices.findById(id);
+    @GetMapping("/{id}")
+    public ResponseEntity<TodoTask> getTaskById(@PathVariable long id) {
+        TodoTask task = todoTaskServices.findById(id);
+        return ResponseEntity.ok(task);
     }
 
+    @PostMapping
+    public ResponseEntity<TodoTask> createTask(@RequestBody Map<String, Object> request) {
+        long creatorId = Long.parseLong(request.get("creatorId").toString());
+        String taskName = request.get("taskName").toString();
 
-//    @PostMapping("/add")
-//    public TodoTask add(@RequestParam long creatorId, @RequestBody String task) {
-//        return todoTaskServices.add(creatorId,task);
-//    }
-//
-//    @PutMapping("/edit")
-//    public TodoTask edit(@RequestParam long id, @RequestBody TodoTask todoTask) {
-//        return todoTaskServices.update(id,todoTask);
-//    }
-//
-//    @DeleteMapping("/remove")
-//    public ResponseEntity<Object> remove(@RequestParam long id) {
-//        return todoTaskServices.delete(id);
-//    }
+        TodoTask task = todoTaskServices.add(creatorId, taskName);
+        return ResponseEntity.ok(task);
+    }
 
+    @PutMapping("/{id}")
+    public ResponseEntity<TodoTask> updateTask(@PathVariable long id, @RequestBody TodoTask todoTask) {
+        TodoTask updatedTask = todoTaskServices.update(id, todoTask);
+        return ResponseEntity.ok(updatedTask);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Object> deleteTask(@PathVariable long id) {
+        return todoTaskServices.delete(id);
+    }
 }
